bde0fdb5472f6c098e87d3eecae8a7d32f81618c
Simon Berger <simon@siku2.io>
2020-05-02 00:15:38 +0200

    add services

--- custom_components/airthingswave/__init__.py
+++ custom_components/airthingswave/__init__.py
@@ -1,12 +1,29 @@
 import logging
 
+from homeassistant.components import mqtt
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType, HomeAssistantType
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType, ServiceCallType
 
 from .const import DOMAIN, KEY_DISCOVERER, PLATFORMS
 from .discovery import DiscoveryHandler
 
 logger = logging.getLogger(__name__)
+
+
+def _register_services(hass: HomeAssistantType) -> None:
+    def send_command(method: str, **kwargs) -> None:
+        payload = kwargs
+        payload["method"] = method
+        mqtt.async_publish(hass, "wave/command", payload)
+
+    async def handle_discover(_call: ServiceCallType) -> None:
+        send_command("discover")
+
+    async def handle_update(call: ServiceCallType) -> None:
+        send_command("update", devices=call.data.get("devices"))
+
+    hass.services.register(DOMAIN, "discover", handle_discover)
+    hass.services.register(DOMAIN, "update", handle_update)
 
 
 async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
@@ -16,6 +33,8 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry) -> bool:
     hass_data = hass.data[DOMAIN]
+    _register_services(hass)
+
     discoverer = hass_data[KEY_DISCOVERER] = DiscoveryHandler(hass, config_entry)
     await discoverer.start()
     return True
